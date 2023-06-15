aa644b6f2d3ae7802f80638d7cf1b437cdff7bf1
adamksmh13 <91722540+adamksmh13@users.noreply.github.com>
2023-06-15 17:31:58 +0700

    Settings base fonts, styles, components and icons

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    fontSize: {
+      "4xl": ["67px", "110%"],
+      "3xl": ["51px", "110%"],
+      "2xl": ["38px", "120%"],
+      xl: ["28px", "140%"],
+      lg: ["22px", "140%"],
+      base: ["16px", "140%"],
+      sm: ["12px", "110%"],
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        sans: ["var(--font-work-sans)"],
+        mono: ["var(--font-space-mono)"],
+      },
+      colors: {
+        prim: "#2B2B2B",
+        sec: "#3B3B3B",
+        caption: "#858584",
+        cta: "#A259FF",
+        blueAcc: "#377DF7",
+        redAcc: "#FF7262",
       },
     },
   },
   plugins: [],
-}
+};
