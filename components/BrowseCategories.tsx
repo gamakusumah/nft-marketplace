ea35ad0d1e6543daeac6fdac6ee7974608dd6bea
adamksmh13 <91722540+adamksmh13@users.noreply.github.com>
2023-06-17 18:01:26 +0700

    Upload category icons and images to directory, Create CategoryCard component and update BrowseCategory component

--- components/BrowseCategories.tsx
+++ components/BrowseCategories.tsx
@@ -1,4 +1,54 @@
+import CategoryCard from "./CategoryCard";
+
 export default function BrowseCategories() {
+  interface Category {
+    name: string;
+    imgSrc: string;
+    iconSrc: string;
+  }
+
+  const categories: Category[] = [
+    {
+      name: "Art",
+      imgSrc: "/images/category/art.jpg",
+      iconSrc: "/icons/category/PaintBrush.svg",
+    },
+    {
+      name: "Collectibles",
+      imgSrc: "/images/category/collectibles.jpg",
+      iconSrc: "/icons/category/Swatches.svg",
+    },
+    {
+      name: "Music",
+      imgSrc: "/images/category/music.jpg",
+      iconSrc: "/icons/category/MusicNotes.svg",
+    },
+    {
+      name: "Photography",
+      imgSrc: "/images/category/photography.jpg",
+      iconSrc: "/icons/category/Camera.svg",
+    },
+    {
+      name: "Video",
+      imgSrc: "/images/category/video.jpg",
+      iconSrc: "/icons/category/VideoCamera.svg",
+    },
+    {
+      name: "Utility",
+      imgSrc: "/images/category/utility.jpg",
+      iconSrc: "/icons/category/MagicWand.svg",
+    },
+    {
+      name: "Sport",
+      imgSrc: "/images/category/sport.jpg",
+      iconSrc: "/icons/category/Basketball.svg",
+    },
+    {
+      name: "Virtual Worlds",
+      imgSrc: "/images/category/virtual-worlds.jpg",
+      iconSrc: "/icons/category/Planet.svg",
+    },
+  ];
   return (
     <section className="container py-10 lg:py-20">
       {/* Headline */}
@@ -9,15 +59,14 @@ export default function BrowseCategories() {
       </div>
 
       {/* Category Cards */}
-      <div className="h-[918px] md:h-[470px] lg:h-[662px] w-full mt-10 lg:mt-[60px] grid grid-cols-2 gap-5 md:grid-cols-4 md:gap-[30px]">
-        <div className="h-[209px] md:h-[231px] lg:h-[316px] bg-sec rounded-[20px]"></div>
-        <div className="h-[209px] md:h-[231px] lg:h-[316px] bg-sec rounded-[20px]"></div>
-        <div className="h-[209px] md:h-[231px] lg:h-[316px] bg-sec rounded-[20px]"></div>
-        <div className="h-[209px] md:h-[231px] lg:h-[316px] bg-sec rounded-[20px]"></div>
-        <div className="h-[209px] md:h-[231px] lg:h-[316px] bg-sec rounded-[20px]"></div>
-        <div className="h-[209px] md:h-[231px] lg:h-[316px] bg-sec rounded-[20px]"></div>
-        <div className="h-[209px] md:h-[231px] lg:h-[316px] bg-sec rounded-[20px]"></div>
-        <div className="h-[209px] md:h-[231px] lg:h-[316px] bg-sec rounded-[20px]"></div>
+      <div className="h-[918px] md:h-[470px] lg:h-[662px] w-full mt-10 lg:mt-[60px] grid grid-cols-2 gap-5 md:grid-cols-4 md:gap-[30px] content-stretch">
+        {categories.map((category, i) => (
+          <CategoryCard
+            name={category.name}
+            imgSrc={category.imgSrc}
+            iconSrc={category.iconSrc}
+          />
+        ))}
       </div>
     </section>
   );
---
+++ components/CategoryCard.tsx
@@ -0,0 +1,35 @@
+import Image from "next/image";
+
+interface CategoryProp {
+  name: string;
+  imgSrc: string;
+  iconSrc: string;
+}
+
+export default function CategoryCard(prop: CategoryProp) {
+  return (
+    <div className="bg-sec rounded-[20px] overflow-hidden">
+      <div className="h-[142px] lg:h-60 w-full overflow-hidden relative">
+        <Image
+          src={prop.imgSrc}
+          width={240}
+          height={240}
+          alt="Category Image"
+          className="object-fit h-full w-full blur-sm"
+        />
+        <div className="w-full h-full absolute top-0 flex items-center justify-center">
+          <Image
+            src={prop.iconSrc}
+            width={100}
+            height={100}
+            alt="Category Icon"
+            className="object-fit h-20 w-20 lg:h-[100px] lg:w-[100px]"
+          />
+        </div>
+      </div>
+      <div className="pt-5 pb-[25px] bg-sec px-5 lg:px-[30px] font-sans font-semibold lg:text-lg items-stretch">
+        <span>{prop.name}</span>
+      </div>
+    </div>
+  );
+}
